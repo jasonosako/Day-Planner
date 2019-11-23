23526ef69947e0ef558ef68fd5794524d2a3cdc2
Jason Osao <josako89@gmail.com>
2019-11-22 20:58:12 -0800

    added color to time slots

--- script.js
+++ script.js
@@ -8,19 +8,54 @@ $(document).ready(function() {
 function setColorBlock() {
     let currentHr = moment().format("HH");
     console.log(moment().format("HH"));
-    for (var i = 09; i < 18; i++) {
-        console.log(currentHr, $("#textarea").data("time"));
-     debugger;
-        
-        }
+    $(".color-rows").each(function() {
+    let colorThis = ($(this).attr("id"));
+    console.log(colorThis);
+        //var color = $(this).attr('id');
+
+    if (currentHr > colorThis) {
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+        $(this).addClass("past");
     };
 
+    if (currentHr === colorThis) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+    };
 
-    setInterval(function() {
-    setColorBlock();
-}, 1000);
-
+    if (currentHr < colorThis) {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("fuure");
+    };
 });
     
+   // var setColor = $(".color-rows").attr("id");
+   // console.log(setColor);
+    //$(".starting-screen").hide();
+    
+   // $(".color-rows").each(function() {
+       // console.log(".color-rows")
+      //  let rowColors = 
+    //})
+   
+      //  console.log(currentHr);
+   
+        //}
+    };
+
+    setInterval (function() {
+    setColorBlock();
+}, 30000);
+
+});
+  
+let saveBtn = $(".saveBtn");
+saveBtn.on("click", function() {
+    let timeId = $(this).attr("id");
+    let textId = $(this).parent().siblings().children(".desription").val();
+    localStorage.setItem(timeId, textId);
+})
 
 
